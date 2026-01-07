46d7157fba03e1da472d4994bed25b2d54d154ee
Rishabh Chatterjee <chatterjeerc2020@gmail.com>
2026-01-07 11:12:53 +0530

    No Change

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -15,7 +15,7 @@ const authMiddleware = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(400).json({ message: 'Invalid token.' });
+    res.status(400).json({ message: 'Ivvaalid token.' });
   }
 };
 
